e5e278a917e1661afc27b6338d542ac7f4ce778c
AA <aalborgil002@gmail.com>
2024-08-14 10:54:48 -0400

    Added seeds but still need to work on them

---
+++ utils/seed.js
@@ -0,0 +1,58 @@
+// Refeactored code but need to check again
+
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const { getRandomName, getRandomReactions } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+    // Delete the collections if they exist
+    let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+    if (userCheck.length) {
+      await connection.dropCollection('users');
+    }
+
+    let thoughtsCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+    if (thoughtsCheck.length) {
+      await connection.dropCollection('thoughts');
+    }
+
+
+  // Create empty array to hold the thoughts
+  const thoughts = [];
+
+  // Loop 20 times -- add thoughts to the thoughts array
+  for (let i = 0; i < 20; i++) {
+    // Get some random reaction objects using a helper function that we imported from ./data
+    const reactions = getRandomReactions(20);
+
+    const fullName = getRandomName();
+    const first = fullName.split(' ')[0];
+    const last = fullName.split(' ')[1];
+    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
+
+    thoughts.push({
+      first,
+      last,
+      github,
+      reactions,
+    });
+  }
+
+  // Add thoughts to the collection and await the results
+  const thoughtData = await Thought.create(thoughts);
+
+  // Add users to the collection and await the results
+  await User.create({
+    userName: 'UCLA',
+    inPerson: false,
+    thoughts: [...thoughtData.map(({_id}) => _id)],
+  });
+
+  // Log out the seed data to indicate what should appear in the database
+  console.table(thoughts);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
